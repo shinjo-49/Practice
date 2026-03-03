678e4c10bf4750123f3c37a222fe81aca34ae318
fashoda49-design <fashoda49@gmail.com>
2026-03-03 12:23:35 +0900

    プロパティの追加とクラス継承の実装

--- Person.cs
+++ Person.cs
@@ -1,25 +1,42 @@
 ﻿namespace Practice;
 
-public class Person
+public class Person//プロパティを通してフィールドにアクセス
 {
-    public string name = "";
-    public int age = 0;
-    public string sex = "回答なし";
-    public void Introduce()
+    protected string name = "";
+    protected int age = 0;
+    protected string sex = "回答なし";
+ 
+    public string Name 
+    {
+        set{name = value;}
+        get{return name;}
+    }
+
+    public int Age
+    {
+        set{age = value;}
+        get{return age;}
+    }
+    
+     public void Introduce()
     {
         Console.WriteLine($"名前：{name} 年齢：{age}歳 性別：{sex}");
     }   
 
-    public void SetInfo(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
-    }
+}
 
+public class Person2 : Person//Personクラスを継承し、新たにメソッドを追加することでPersonクラスの呼び出し機能を拡張
+{
     public void SetInfo(string name, int age, string sex)
     {
         this.name = name;
         this.age = age;
         this.sex = sex;
     }
+    public void SetInfo(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+       
 }
--- Program.cs
+++ Program.cs
@@ -6,18 +6,21 @@ class Program
 {
     static void Main()
     {
-        Person p1,p2,p3;
-        p1 = new Person();
-        p2 = new Person();
-        p3 = new Person();
+        Person2 p1,p2,p3,p4;
+        p1 = new Person2();
+        p2 = new Person2();
+        p3 = new Person2();
+        p4 = new Person2();
         
 
-        p1.name = "山田太郎";//フィールドに直接アクセスして値を代入
-        p1.age = 20;
+        p1.Name = "山田太郎";//フィールドに直接アクセスして値を代入ができないため（できないようにした）、プロパティを呼び出して値を代入
+        p1.Age = 20;
 
-        p2.SetInfo("田中花子", 25);//メソッドを呼び出して値を代入
+        p2.SetInfo("田中花子", 25);//メソッドを呼び出して値を代入(オーバーロード、性別なし)
 
-        p3.SetInfo("佐藤次郎", 30, "男性");//メソッドを呼び出して値を代入
+        p3.SetInfo("佐藤次郎", 30, "男性");//メソッドを呼び出して値を代入（オーバーロード、性別あり）
+
+    
 
         p1.Introduce();
         p2.Introduce();
