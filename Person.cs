ce8c78bec6daf26128a852108799e137ed32bb52
fashoda49-design <fashoda49@gmail.com>
2026-03-03 11:46:37 +0900

    オーバーロード実装

--- Person.cs
+++ Person.cs
@@ -4,14 +4,22 @@ public class Person
 {
     public string name = "";
     public int age = 0;
+    public string sex = "回答なし";
     public void Introduce()
     {
-        Console.WriteLine($"名前：{name} 年齢：{age}歳");
+        Console.WriteLine($"名前：{name} 年齢：{age}歳 性別：{sex}");
     }   
 
-    public void SetAgeAndName(string name, int age)
+    public void SetInfo(string name, int age)
     {
         this.name = name;
         this.age = age;
+    }
+
+    public void SetInfo(string name, int age, string sex)
+    {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
+using System.Security.Cryptography;
 using Practice;
 
 class Program
 {
     static void Main()
     {
-        Person p1,p2;
+        Person p1,p2,p3;
         p1 = new Person();
         p2 = new Person();
+        p3 = new Person();
+        
 
         p1.name = "山田太郎";//フィールドに直接アクセスして値を代入
         p1.age = 20;
 
-        p2.SetAgeAndName("田中花子", 25);//メソッドを呼び出して値を代入
+        p2.SetInfo("田中花子", 25);//メソッドを呼び出して値を代入
+
+        p3.SetInfo("佐藤次郎", 30, "男性");//メソッドを呼び出して値を代入
 
         p1.Introduce();
         p2.Introduce();
+        p3.Introduce();
     }
 }
