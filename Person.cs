dbb099b755823c98677b50208d4c9dde9402f34f
fashoda49-design <fashoda49@gmail.com>
2026-03-03 11:20:06 +0900

    人クラスと実行クラス

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice;
+
+public class Person
+{
+    public string name = "";
+    public int age = 0;
+    public void Introduce()
+    {
+        Console.WriteLine($"名前：{name} 年齢：{age}歳");
+    }   
+
+    public void SetAgeAndName(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Practice;
+
+class Program
+{
+    static void Main()
+    {
+        Person p1,p2;
+        p1 = new Person();
+        p2 = new Person();
+
+        p1.name = "山田太郎";
+        p1.age = 20;
+
+        p2.SetAgeAndName("田中花子", 25);
+
+        p1.Introduce();
+        p2.Introduce();
+    }
+}
