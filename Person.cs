a035268e3a28ffe7336096cc96ccbe8ba23f7cf4
fashoda49-design <fashoda49@gmail.com>
2026-03-03 13:24:29 +0900

    自動実装プロパティと初期値ありのコンストラクタ

--- Person.cs
+++ Person.cs
@@ -1,33 +1,33 @@
 ﻿namespace Practice;
 
-public class Person//プロパティを通してフィールドにアクセス
+public class Person
 {
-    protected string name = "";
-    protected int age = 0;
-    protected string howToMove = "こんな風に動かします";
-
-    protected int password = 0000;
- 
-    public string Name 
+        public Person(string name = "名無し", int age = 0, int password = 0000)
     {
-        set{name = value;}
-        get{return name;}
+        Name = name;
+        Age = age;
+        HowToMove = "歩く";
+        PassWord = password;
+    }
+ 
+    public string Name //自動実装プロパティは、フィールドを自動的に生成してくれるプロパティのことだが、フィールドと違って初期値がないため、nullになる可能性がある
+    {
+        set;get;
     }
 
     public int Age
     {
-        set{age = value;}
-        get{return age;}
+        set;get;
     }
 
     public int PassWord
     {
-        set{password = value;}
+        set;private get;
     }
 
     public string HowToMove
     {
-        get{return howToMove;}
+        get;private set;
     }
     
      /*
